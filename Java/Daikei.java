e36cbbd21051e89e0d86f64d1fc64ca57884a3dd
TU182592021 <147878282+TU182592021@users.noreply.github.com>
2024-03-04 19:48:38 +0900

    Add files via upload

---
+++ Java/Daikei.java
@@ -0,0 +1,6 @@
+public class Daikei {
+    public static void main(String[] args) {
+        System.out.println((3 + 2) * 4 / 2);
+        System.out.println(100000L * 100000L);
+    }
+}
---
+++ Java/Kantan.java
@@ -0,0 +1,10 @@
+public class Kantan {
+    public static void main(String[] args) {
+        System.out.println("以外に\"java\"って簡単だ\n以外に\"java\"って簡単だ\n以外に\"java\"って簡単だ");
+        System.out.println("加算の練習" + 3 + 2 + "です");
+        System.out.println(2 - 3 * 4);
+        System.out.println(6 - 5 / 4);
+        System.out.printf("加算の結果は%dです。\n", 3 + 2);
+    }
+
+}
